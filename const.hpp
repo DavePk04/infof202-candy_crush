82f590ea9c32f613e46f023e0d191f232699d026
BDMR21 <ben.david.malyane@ulb.be>
2022-08-20 14:47:50 +0200

    ICE IMPLEMENTATION AND LEVELS LOGIQUE'S BASE

--- const.hpp
+++ const.hpp
@@ -12,7 +12,7 @@ const int GRID_DIMENSION = 9;
 const int TOTALCOLOR = 6;
 const int MIN_CANDIES_ALIGNED = 3;
 const int CANDY_DELETED = 0;
-enum COLORS {BLUE=1, RED=2, GREEN=3, YELLOW=4, CYAN=5, MAGENTA=6, DARK_YL = 7, BLACK = 8};
+enum COLORS {BLUE=1, RED=2, GREEN=3, YELLOW=4, CYAN=5, MAGENTA=6, DARK_YL = 7, BLACK = 8, GRAY=9, DGRAY=10};
 const vector<int> COLORS_VECT{1,2,3,4,5,6,7};
 const vector<Point> DIR{{0,-1},{0,1},{-1,0},{1,0}};
 const vector<Point> BOMBDIR{{0,-1},{0,1},{-1,0},{1,0}, {1,1}, {1, -1},
